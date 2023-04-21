77a421dca3d3582635912a1efce9c8cbcae12763
comedicomedy <bsinex389@gmail.com>
2023-04-21 10:03:50 -0400

    INIT

---
+++ cmake-build-debug/_deps/raylib-build/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/bsine/Dropbox/PC/Desktop/Projects/Libary Games/PizzJam/cmake-build-debug/_deps/raylib-src/src
+# Build directory: C:/Users/bsine/Dropbox/PC/Desktop/Projects/Libary Games/PizzJam/cmake-build-debug/_deps/raylib-build/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
