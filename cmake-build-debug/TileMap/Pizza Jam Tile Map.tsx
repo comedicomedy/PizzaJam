d04388bb40c6e8e886d3798cb3d2b27a05d757c7
comedicomedy <bsinex389@gmail.com>
2023-04-25 20:04:53 -0400

    WIP 3

---
+++ cmake-build-debug/TileMap/Pizza Jam Tile Map.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="Pizza Jam Tile Map" tilewidth="150" tileheight="150" tilecount="84" columns="12">
+ <image source="PizzaJamTileMap.png" width="1920" height="1080"/>
+</tileset>
--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 #include "raylib.h"
 #include "string"
+#include "tmx.h"
+#define RAYLIB_TMX_IMPLEMENTATION
+#include "raylib-tmx.h"
+
 
 class player{
     public:
@@ -15,24 +19,27 @@ class player{
 
     void control(){
         if (IsKeyDown(KEY_W) && pos.y >= 0) pos.y -= speed;
-        if (IsKeyDown(KEY_S) && pos.y < 600-35) pos.y += speed;
-        if (IsKeyDown(KEY_D) && pos.x < 800-21) pos.x += speed;
+        if (IsKeyDown(KEY_S) && pos.y < 1080-35) pos.y += speed;
+        if (IsKeyDown(KEY_D) && pos.x < 1920-21) pos.x += speed;
         if (IsKeyDown(KEY_A) && pos.x >= 0) pos.x -= speed;
     }
 
 };
 
+
+
 int main(void) {
-    const int screenWidth = 800;
-    const int screenHeight = 600;
-    auto _player =  player(2.5);
+    const int screenWidth = 1920;
+    const int screenHeight = 1080;
+    auto _player =  player(10);
     int time = 5;
 
     InitWindow(screenWidth,screenHeight, "Pizza Jam");
     _player.pos = {screenWidth/2-21/2, screenHeight/2-35/2};
 
     //Loading Texture
-    Texture2D playerSprite = LoadTexture("Sprites/BombBoy.png");
+    Texture2D playerSpriteLit = LoadTexture("Sprites/BombBoyV2Lit");
+    tmx_map* testLevel = LoadTMX("TestMap.tmx");
 
     SetTargetFPS(60);
     while (!WindowShouldClose()){
@@ -43,13 +50,14 @@ int main(void) {
         BeginDrawing();
             ClearBackground(RAYWHITE);
 
-            DrawTexture(playerSprite, _player.pos.x, _player.pos.y,WHITE);
+            DrawTexture(playerSpriteLit, _player.pos.x, _player.pos.y,WHITE);
 
             DrawText(std::to_string(GetTime()).data(),0,0,40,LIGHTGRAY);
+            DrawTMX(testLevel, 0,0,WHITE);
         EndDrawing();
     }
 
-    UnloadTexture(playerSprite);
+    UnloadTexture(playerSpriteLit);
 
     CloseWindow();
 
